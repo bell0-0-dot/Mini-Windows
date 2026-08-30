402efeabaf65312caa22f7f43565122091f8c7ce
bell0-0-dot <bella_serrano@unitec.edu>
2026-08-30 16:45:05 -0600

    Cuerpo de la clase Usuario(base)

--- src/base/Usuario.java
+++ src/base/Usuario.java
@@ -1,16 +1,68 @@
-/*
- * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
- * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
- */
+
 package base;
 
 import java.io.Serializable;
+import java.time.LocalDate;
 
-/**
- *
- * @author gabri
- */
 
-public class Usuario implements Serializable{
+
+public  class Usuario implements Serializable{
+    protected String User;
+    protected String password;
+    protected boolean activo;
+    protected LocalDate fechaRegistro;
+
+    public Usuario(String User, String password) {
+        this.User = User;
+        this.password = password.trim();
+        this.activo=true;
+        this.fechaRegistro=LocalDate.now();
+        
+    }
+    public boolean verificarPassword(String intento){
+        if(intento==null){
+            return false;
+        }
+        String ingresado=intento.trim();
+        
+        return ingresado.equals(password);
+    }
+
+    public void activarUser(){
+        this.activo=true;
+        
+    }
+    public void desactivarUser(){
+        this.activo=false;
+        
+    }
+    public String getUser() {
+        return User;
+    }
+
+    public void setUser(String User) {
+        this.User = User;
+    }
+
+    public boolean isActivo() {
+        return activo;
+    }
+
+    public void setActivo(boolean activo) {
+        this.activo = activo;
+    }
+
+    public LocalDate getFechaRegistro() {
+        return fechaRegistro;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
+    
     
 }
